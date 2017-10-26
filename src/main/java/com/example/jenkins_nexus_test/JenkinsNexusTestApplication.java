5b298c5c56ee212407ab69dc5438a5e7a4983539
bartasondra <bartasondra@seznam.cz>
2017-10-26 13:55:03 +0200

    test project for jenkins

---
+++ src/main/java/com/example/jenkins_nexus_test/JenkinsNexusTestApplication.java
@@ -0,0 +1,12 @@
+package com.example.jenkins_nexus_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsNexusTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsNexusTestApplication.class, args);
+	}
+}
